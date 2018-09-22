9a57aa1c86359e9133b962d14ef2360f565f5558
ll9 <llwierer@web.de>
2018-09-22 14:29:00 +0200

    Added SDSquare style

--- src/data/style.js
+++ src/data/style.js
@@ -6,15 +6,30 @@ import {
 } from 'ol/style'
 
 
-const stroke = new Stroke({color: 'white', width: 2});
-const fill = new Fill({color: 'blue'});
+const stroke = new Stroke({
+    color: 'white',
+    width: 2
+});
+const fill = new Fill({
+    color: 'blue'
+});
 
 export const SDCircle = new Style({
-        image: new RegularShape({
-            fill: fill,
-            stroke: stroke,
-            points: 100,
-            radius: 10,
-            angle: 0
-        })
-    });
+    image: new RegularShape({
+        fill: fill,
+        stroke: stroke,
+        points: 100,
+        radius: 10,
+        angle: 0
+    })
+});
+
+export const SDSquare = new Style({
+    image: new RegularShape({
+        fill: fill,
+        stroke: stroke,
+        points: 4,
+        radius: 10,
+        angle: 0
+    })
+});
