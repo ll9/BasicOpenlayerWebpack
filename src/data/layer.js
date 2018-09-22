f59035ae82aec2faa8d399b725bf0d2d45f58f2a
ll9 <llwierer@web.de>
2018-09-22 15:53:47 +0200

    Basic Cluster Classification

--- src/data/layer.js
+++ src/data/layer.js
@@ -2,8 +2,8 @@ import VectorSource from 'ol/source/Vector';
 import Cluster from 'ol/source/Cluster'
 import GeoJSON from 'ol/format/GeoJSON';
 import VectorLayer from 'ol/layer/Vector';
-import { SDSquare, SDCircle } from './style';
-import Feature from 'ol/Feature';
+import { SDstyleCluster } from './style';
+
 
 let vectorSource = new VectorSource({
     url: "/src/data/geodata.json",
@@ -14,28 +14,8 @@ let clusterSource = new Cluster({source: vectorSource})
 
 let vectorLayer = new VectorLayer({
     source: clusterSource,
-    style: stylefun //SDSquare
+    style: SDstyleCluster //SDSquare
 })
 
-/**
- * 
- * @param {Feature} cluster 
- * @param {number} resolution 
- */
-function stylefun(cluster, resolution) {
-    /**
-     * @type {Feature[]} features
-     */
-    let features = cluster.getProperties().features;
-
-    let occurences = features.map(feature => feature.getProperties().straße)
-    let mostFrequent = occurences.sort((a,b) =>
-          occurences.filter(v => v===a).length
-        - occurences.filter(v => v===b).length
-    ).pop();
-
-    if (mostFrequent == "Kampenwandstraße") return [SDSquare]
-    else if (mostFrequent == "Scheibenwandstraße") return [SDCircle]
-}
 
 export default vectorLayer;
--- src/data/style.js
+++ src/data/style.js
@@ -33,3 +33,24 @@ export const SDSquare = new Style({
         angle: 0
     })
 });
+
+/**
+ * 
+ * @param {Feature} cluster 
+ * @param {number} resolution 
+ */
+export function SDstyleCluster(cluster, resolution) {
+    /**
+     * @type {Feature[]} features
+     */
+    let features = cluster.getProperties().features;
+
+    let occurences = features.map(feature => feature.getProperties().straße)
+    let mostFrequent = occurences.sort((a,b) =>
+          occurences.filter(v => v===a).length
+        - occurences.filter(v => v===b).length
+    ).pop();
+
+    if (mostFrequent == "Kampenwandstraße") return [SDSquare]
+    else if (mostFrequent == "Scheibenwandstraße") return [SDCircle]
+}
